85591b44857240a5915cb6c459b921d9ee6f623f
Bastian Isensee <bisensee@bastian-isensee.com>
2025-06-12 20:58:58 +0200

    Refactor types and structure, prevent moves with collision

---
+++ burger/burger.go
@@ -0,0 +1,120 @@
+package burger
+
+import (
+	"fmt"
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"log"
+	"math/rand/v2"
+)
+
+// Ingredient defines common properties of a particular Burger Part, e.g. the name and image of a tomato or cheese layer.
+type Ingredient struct {
+	name  string
+	image *ebiten.Image
+}
+
+var allIngredients []Ingredient
+
+func init() {
+	allIngredients = []Ingredient{
+		{name: "bun_bottom"},
+		{name: "tomatoes"},
+		{name: "patty_beef"},
+		{name: "ketchup"},
+		{name: "salad"},
+		{name: "patty_vegan"},
+		{name: "mayo"},
+		{name: "cheese"},
+		{name: "ham"},
+		{name: "onions"},
+		{name: "pickles"},
+		{name: "bun_top"},
+	}
+
+	for index, ingredient := range allIngredients {
+		path := fmt.Sprintf("%s/%s.png", imageBasePath, ingredient.name)
+		image, _, err := ebitenutil.NewImageFromFile(path)
+
+		if err != nil {
+			log.Fatalf("Error while loading ingredient image %s: %v", ingredient.name, err)
+		}
+
+		allIngredients[index].image = image
+	}
+}
+
+// Part is a specific instances of an ingredient with a specific position on the screen. Note that there can be multiple
+// Parts of the same Ingredient, but at different locations on the screen, e.g. multiple pickle instances.
+type Part struct {
+	ingredient Ingredient
+	lane       int
+	x          int
+	y          int
+}
+
+func newPart(ingredient Ingredient) *Part {
+	// Spawn the Part in the middle lane.
+	lane := laneCount / 2
+
+	return &Part{
+		ingredient: ingredient,
+		lane:       lane,
+		x:          lane * laneWidth,
+		y:          0,
+	}
+}
+
+func newRandomPart() *Part {
+	if len(allIngredients) == 0 {
+		log.Fatal("Ingredients not initialized yet, cannot create random part")
+	}
+	randomIngredient := allIngredients[rand.IntN(len(allIngredients))]
+
+	return newPart(randomIngredient)
+}
+
+func (p *Part) move(x, y int) {
+	p.x = x
+	p.y = y
+}
+
+func (p *Part) height() int {
+	// Account for the scale factor when calculating the images height.
+	return int(float64(p.ingredient.image.Bounds().Size().Y) * scaleFactor)
+}
+
+func (p *Part) draw(screen *ebiten.Image) {
+	opts := &ebiten.DrawImageOptions{}
+	opts.GeoM.Scale(scaleFactor, scaleFactor)
+	opts.GeoM.Translate(float64(p.x), float64(p.y))
+
+	screen.DrawImage(p.ingredient.image, opts)
+}
+
+// Burger is a stack of Part instances, i.e. specific ingredients at a certain location ordered from bottom to top.
+type Burger struct {
+	stack []*Part
+}
+
+func newBurger() *Burger {
+	return &Burger{stack: make([]*Part, 0)}
+}
+
+func (b *Burger) top() *Part {
+	if len(b.stack) == 0 {
+		return nil
+	}
+
+	return b.stack[len(b.stack)-1]
+}
+
+func (b *Burger) add(part *Part) {
+	b.stack = append(b.stack, part)
+}
+
+func (b *Burger) draw(screen *ebiten.Image) {
+	for _, part := range b.stack {
+		part.draw(screen)
+	}
+}
---
+++ burger/game.go
@@ -0,0 +1,138 @@
+package burger
+
+import (
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
+	_ "image/png"
+)
+
+const (
+	ScreenWidth  = 1200
+	ScreenHeight = 1200
+
+	defaultFallStep = 5
+	fastFallStep    = defaultFallStep * 4
+	scaleFactor     = 0.4
+	laneCount       = 3
+	laneWidth       = ScreenWidth / laneCount
+
+	imageBasePath = "resources/images/ingredients/png/"
+)
+
+type Game struct {
+	// falling is the currently moving ingredient Part that has not yet landed on a specific Burger.
+	falling *Part
+	// burgers represents multiple stacks of layered ingredient Parts, ordered from left to right (lanes).
+	burgers []*Burger
+}
+
+func (g *Game) move(targetLane int, stepSize int) {
+	if g.falling == nil {
+		// Piling finished, nothing to update.
+		return
+	}
+
+	if targetLane < 0 || targetLane >= len(g.burgers) {
+		// Refuse changing the lane as we are at the bounds of the screen.
+		targetLane = g.falling.lane
+	}
+
+	// By default, if there is no started burger yet, the ingredient part should stop moving at the lower bottom.
+	partHeight := g.falling.height()
+	maxY := ScreenHeight - partHeight - 1
+
+	targetBurger := g.burgers[targetLane]
+	topPart := targetBurger.top()
+
+	if topPart != nil {
+		// There is an existing burger, stop moving when we are halfway over the top most part (allow some overlay).
+		maxY = topPart.y - (partHeight / 2)
+	}
+
+	if maxY < 0 {
+		// Finish the piling and spawning process.
+		g.falling = nil
+
+		return
+	}
+
+	if g.falling.y > maxY {
+		// There is no more space in this Burgers lane to add new Parts. We need to prevent a collision.
+		// Refuse changing the lane and move normally if a vertical move was initiated.
+		// Else finish the piling and spawning process.
+		if targetLane == g.falling.lane {
+			// Finish the piling and spawning process.
+			g.falling = nil
+
+			return
+		}
+
+		// TODO: Check if we can avoid recursive calls here and make it more simple.
+		g.move(g.falling.lane, stepSize)
+
+		return
+	}
+
+	// Accept the lane change if present as we found no collision with existing Burgers.
+	g.falling.lane = targetLane
+
+	// Move our falling Part one step further down as we have not hit the bottom or Burger top yet.
+	g.falling.move(g.falling.lane*laneWidth, g.falling.y+stepSize)
+
+	if g.falling.y > maxY {
+		// We hit the bottom or the top most Part of a Burger. Add the layer to the burger and spawn a new Part.
+		g.falling.move(g.falling.x, maxY)
+		targetBurger.add(g.falling)
+		g.falling = newRandomPart()
+	}
+}
+
+func NewGame() *Game {
+	burgers := make([]*Burger, laneCount)
+
+	for index := range laneCount {
+		burgers[index] = newBurger()
+	}
+
+	return &Game{
+		falling: newRandomPart(),
+		burgers: burgers,
+	}
+}
+
+func (g *Game) Update() error {
+	if inpututil.IsKeyJustPressed(ebiten.KeyEscape) {
+		// Signal that the game shall terminate normally.
+		return ebiten.Termination
+	}
+	if g.falling == nil {
+		// Piling finished, nothing to update.
+		return nil
+	}
+
+	if inpututil.IsKeyJustPressed(ebiten.KeyLeft) {
+		g.move(g.falling.lane-1, defaultFallStep)
+	} else if inpututil.IsKeyJustPressed(ebiten.KeyRight) {
+		g.move(g.falling.lane+1, defaultFallStep)
+	} else if inpututil.KeyPressDuration(ebiten.KeyDown) > 0 {
+		g.move(g.falling.lane, fastFallStep)
+	} else {
+		g.move(g.falling.lane, defaultFallStep)
+	}
+
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	if g.falling != nil {
+		g.falling.draw(screen)
+	}
+
+	for _, burger := range g.burgers {
+		burger.draw(screen)
+	}
+}
+
+func (g *Game) Layout(width, height int) (screenWidth, screenHeight int) {
+	return width, height
+}
